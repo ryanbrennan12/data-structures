7931f9da052b5913b75c32edb016b6b19ad6166c
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-29 12:18:03 -0700

    First test passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,17 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj = _.extend(obj, stackMethods);
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
