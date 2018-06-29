c2699018b0ff1aa4e38160331819ef99a408e29e
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-29 13:24:57 -0700

    Size function complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {}
+  obj = _.extend(obj, queueMethods);
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
