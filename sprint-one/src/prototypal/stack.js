a61c67549e278aabd0ca881220b1ecc4eedda6b7
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 11:53:29 -0700

    Push function working

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,6 +3,7 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var obj = Object.create(stackMethods);
   obj.storage = {};
+  obj.key = 0;
 
   return obj;
 };
@@ -10,6 +11,12 @@ var Stack = function() {
 var stackMethods = {
 	size: function() {
 		return Object.keys(this.storage).length;
+	},
+
+	push: function(value) {
+		this.storage[this.key] = value;
+		this.key ++;
+
 	}
 };
 
