97abbeb80b091be776fbd77ca34f4b80880c5132
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 12:35:29 -0700

    Stack Completed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -4,6 +4,20 @@ var Stack = function() {
 
 };
 
-Stack.prototype.size= function() {
+Stack.prototype.size = function() {
 	return Object.keys(this.storage).length
+}
+
+Stack.prototype.push = function(value) {
+	this.storage[this.key] = value;
+	this.key ++;
+}
+
+Stack.prototype.pop = function() {
+	var arr = Object.keys(this.storage);
+	var last = arr[arr.length - 1];
+	var popped = this.storage[last];
+	delete this.storage[last];
+
+	return popped;
 }
