9ff55688aee2d8822e4a3bf1e066e277f852eae9
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 12:05:26 -0700

    Enqueue function working

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,6 +3,7 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var obj = Object.create(queueMethods);
   obj.storage = {};
+  obj.key = 0;
 
   return obj;
 };
@@ -10,6 +11,11 @@ var Queue = function() {
 var queueMethods = {
 	size: function() {
 		return Object.keys(this.storage).length;
+	},
+
+	enqueue: function(value) {
+		this.storage[this.key] = value;
+		this.key ++;
 	}
 };
 
