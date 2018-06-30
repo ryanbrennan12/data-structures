7e302668c0a8ec148a282fe028be95bad787113d
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 11:49:26 -0700

    Size function working on both

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,16 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
