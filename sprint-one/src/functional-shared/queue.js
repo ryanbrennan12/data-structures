8fa4cf60366d086af5f32225f895bd23914b0577
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-29 23:17:52 -0700

    Dequeue implemented. All tests passing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,9 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var obj = {};
   obj.storage = {}
+
   obj = _.extend(obj, queueMethods);
+  obj.key = 0;
 
   return obj;
 };
@@ -11,6 +13,21 @@ var Queue = function() {
 var queueMethods = {
 	size: function() {
 		return Object.keys(this.storage).length;
+	},
+
+	enqueue: function(value) {
+		this.storage[this.key] = value;
+		this.key ++;
+	},
+
+	dequeue: function() {
+		var arr = Object.keys(this.storage); // [0,1]
+		var first = arr[0] // [0]
+		var popped = this.storage[first];
+
+		delete this.storage[first];
+
+		return popped;
 	}
 };
 
