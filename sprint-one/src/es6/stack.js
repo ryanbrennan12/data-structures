621e4f5eea96daca7c445caaa53e4c09b8a5443c
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 19:41:31 -0700

    Size and Push implemented

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,17 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.key = 0;
   }
-
+  size() {
+    return Object.keys(this.storage).length;
+  }
+  push(value) {
+    this.storage[this.key] = value;
+    this.key ++;
+  }
+  pop() {
+    
+  }
 }
