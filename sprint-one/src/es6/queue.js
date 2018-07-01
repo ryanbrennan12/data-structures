4b951955aa233b55b7d30732f46fc20aaf5564e8
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-06-30 19:29:40 -0700

    Size function running

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,12 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  	this.key = 0;
   }
+  size() {
+  	return Object.keys(this.storage).length;
+  }
+  
 
 }
